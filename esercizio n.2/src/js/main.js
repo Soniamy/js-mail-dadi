bab25ddf83eab624ace772b54be0acdd8e56a18d
Soniamy <marianna.marzullo@gmail.com>
2023-12-06 17:36:25 +0100

    scomposizione letteraria del problema

---
+++ esercizio n.2/src/js/main.js
@@ -0,0 +1,5 @@
+// 1.Generare un numero random da 1 a 6 per il giocatore
+// 2.Generare un numero random da 1 a 6 per il giocatore
+// 3.Se il punteggio del giocatore è maggiore nel punteggio del computer allora ha vinto il giocatore
+// 4.Se il punteggio del giocatore è uguale nel punteggio del computer allora il punteggio è pari
+// 5.Se il punteggio del giocatore è minore nel punteggio del computer allora ha vinto il computer
