80a014390b818b1ae5b843a41c1299ffc694f5c1
Soniamy <marianna.marzullo@gmail.com>
2023-12-06 15:40:58 +0100

    scomposizione dell'esercizio

---
+++ esercizio n.1/src/js/main.js
@@ -0,0 +1,5 @@
+//1.Chiedere all'utente la sua email,
+//2. Controllare che sia nella lista di chi può accedere
+//3. Se l'email dell'utente è presente nella lista allora può accedere
+//4. Altrimenti non può accedere
+//5.Stampare un messaggio appropriato sull'esito del controllo.
